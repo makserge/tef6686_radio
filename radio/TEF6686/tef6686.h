b6b4d6c15f5471218c8abbdab72cc428c1f70276
Makogon Sergey <SMakogon@luxoft.com>
2018-10-25 19:45:04 +0300

    Initial commit

---
+++ radio/TEF6686/tef6686.h
@@ -0,0 +1,21 @@
+#ifndef TEF6686_H
+#define TEF6686_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include "Tuner_Api.h"
+#include "Tuner_Proc.h"
+#include "Tuner_Api_Lithio.h"
+#include "Tuner_Drv_Lithio.h"
+#include "Tuner_Interface.h"
+
+#define I2C_ADDR 0x64
+
+#endif
+
